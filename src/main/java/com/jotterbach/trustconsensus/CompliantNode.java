18c24d2b07991b71efb78413d46489865318b39e
Johannes Otterbach <johannesotterbach@gmail.com>
2017-10-24 09:06:27 -0700

    Add simple blacklist

--- src/main/java/com/jotterbach/trustconsensus/CompliantNode.java
+++ src/main/java/com/jotterbach/trustconsensus/CompliantNode.java
@@ -1,29 +1,75 @@
 package com.jotterbach.trustconsensus;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private double p_graph;
+    private double p_malicious;
+    private double p_txDistribution;
+    private int numRounds;
+
+    private boolean[] followees;
+
+    private Set<Transaction> pendingTransactions = new HashSet<>();
+
+    private Set<Integer> blacklist = new HashSet<>();
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        Set<Transaction> unseenTxn = pendingTransactions
+                .stream()
+                .filter(tx -> !this.pendingTransactions.contains(tx))
+                .collect(Collectors.toSet());
+        this.pendingTransactions.addAll(unseenTxn);
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        Set<Transaction> txnToSend = new HashSet<>(this.pendingTransactions);
+        this.pendingTransactions.clear();
+        return txnToSend;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        updateBlacklist(candidates);
+        Set<Candidate> validCandidates = candidates
+                .stream()
+                .filter(candidate -> this.followees[candidate.sender] && !this.blacklist.contains(candidate.sender))
+                .collect(Collectors.toSet());
+
+        validCandidates.forEach(candidate -> this.pendingTransactions.add(candidate.tx));
     }
+
+    private void updateBlacklist(Set<Candidate> candidates) {
+        // Need to blacklist nodes that signed up as a follower, but do not send anything.
+        // Strategy:
+        // 1. Get the IDs of all senders
+        // 2. Loop through the followees and identify those who are in the group of followees but did not submit a
+        //    candidate transaction.
+        // 3. Add those to a blacklist
+
+        Set<Integer> senderIds = candidates
+                .stream()
+                .map(candidate -> candidate.sender)
+                .collect(Collectors.toSet());
+        for (int i = 0; i < this.followees.length; i++) {
+            if (this.followees[i] && !senderIds.contains(i)) {
+                this.blacklist.add(i);
+            }
+        }
+    }
+
 }
